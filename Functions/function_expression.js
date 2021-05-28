ed073e42d6a874bd598f2f893ad97537c95290a3
Ajay Mathe <ajay.mathe9@gmail.com>
2021-05-28 12:16:14 +0530

    Function expression

---
+++ Functions/function_expression.js
@@ -0,0 +1,7 @@
+//functions stored in a variable are called function expressions
+
+let calculateSavings = function (totalIncome, expense) {
+    return totalIncome - expense
+}
+
+console.log(calculateSavings(500000, 220000));
